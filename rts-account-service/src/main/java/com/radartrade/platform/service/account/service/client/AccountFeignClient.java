f8d4d91ec33e97de11792545238953c1abd67364
devsirose <dev.sirose@gmail.com>
2025-07-10 20:53:13 +0700

    modified AccountFeignClient.class

--- rts-account-service/src/main/java/com/radartrade/platform/service/account/service/client/AccountFeignClient.java
+++ rts-account-service/src/main/java/com/radartrade/platform/service/account/service/client/AccountFeignClient.java
@@ -2,13 +2,14 @@ package com.radartrade.platform.service.account.service.client;
 
 import com.radartrade.platform.service.common.dto.response.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("${spring.application.name}")
 public class AccountFeignClient {
-    @GetMapping(value = "/api/v1/account", consumes = "application/json")
+    @GetMapping(value = "/api/v1/account", consumes = {MediaType.APPLICATION_JSON_VALUE})
     ResponseEntity<BaseResponse> getResponse(@RequestParam Long accountId) {
         return null;
     }
