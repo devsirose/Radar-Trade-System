1f62163533566ad41589df8661bd419d31e9b4ba
devsirose <dev.sirose@gmail.com>
2025-07-16 17:26:46 +0700

    config authorization code grant type flow

--- rts-api-gateway/src/main/java/com/radartrade/platform/server/gateway/config/KeycloakRoleConverter.java
+++ rts-api-gateway/src/main/java/com/radartrade/platform/server/gateway/config/KeycloakRoleConverter.java
@@ -1,4 +1,37 @@
 package com.radartrade.platform.server.gateway.config;
 
-public class KeycloakRoleConverter {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class KeycloakRoleConverter  implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return new ArrayList<>();
+        }
+        Collection<GrantedAuthority> grantedAuthorities = ((List<String>) realmAccess.get("roles"))
+                .stream().map(this::normalizeRoleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return grantedAuthorities;
+    }
+
+    private String normalizeRoleName(String role) {
+        if (role.startsWith("ROLE_"))
+            return role;
+        return new StringBuilder().append("ROLE_").append(role).toString();
+    }
+
 }
