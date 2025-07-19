d9b0cd55575d58ab4db37cb6ba3595c2239bbddd
devsirose <dev.sirose@gmail.com>
2025-07-19 18:13:50 +0700

    configured application.yml: connect to timescaleDB

--- rts-exchange-gateway-processor/src/main/java/com/radartrade/platform/service/exchangeprocessor/RTSExchangeProcessorApplication.java
+++ rts-exchange-gateway-processor/src/main/java/com/radartrade/platform/service/exchangeprocessor/RTSExchangeProcessorApplication.java
@@ -8,11 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RTSExchangeProcessorApplication {
     public static void main(String[] args) {
         SpringApplication.run(RTSExchangeProcessorApplication.class, args);
-        PriceConsumer client = new PriceConsumer();
-
-        client.priceUpdatesStream()
-                .subscribe(update -> System.out.printf("Symbol: %s, Price: %s, Time: %d%n",
-                        update.getSymbol(), update.getPrice(), update.getEventTime()));
+//        PriceConsumer client = new PriceConsumer();
+//
+//        client.priceUpdatesStream()
+//                .subscribe(update -> System.out.printf("Symbol: %s, Price: %s, Time: %d%n",
+//                        update.getSymbol(), update.getPrice(), update.getEventTime()));
 
     }
 }
